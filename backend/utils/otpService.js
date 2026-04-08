ebcd15f47f73295d884c01377794ae0f4e023b31
Oman21-skanic <muhammadrohman692@gmail.com>
2026-04-08 21:44:34 +0700

    otp fiks

--- backend/utils/otpService.js
+++ backend/utils/otpService.js
@@ -49,7 +49,9 @@ async function sendOTPEmail(toEmail, otp) {
   }
 
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: { user: gmailUser, pass: gmailPass },
   });
 
