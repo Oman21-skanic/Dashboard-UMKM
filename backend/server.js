3c0c55ad9890ede0d28d801b934473b70909f989
Oman21-skanic <muhammadrohman692@gmail.com>
2026-05-11 08:33:57 +0700

    feat: initialize server and establish MongoDB connection in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  dbName: 'dashboard-umkm'
+})
   .then(() => {
     console.log('Koneksi database berhasil terhubung.');
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log('Server berjalan pada port ' + PORT);
     });
   })
